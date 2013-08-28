94e9646d0bcba3617073842a372431de95f7631c
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-08-28 10:24:03 -0700

    Added XML Parser to the repo and some parsing

--- src/com/nguyenmp/starclient/STAR.java
+++ src/com/nguyenmp/starclient/STAR.java
@@ -4,6 +4,8 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import java.io.IOException;
 
@@ -12,18 +14,22 @@ import java.io.IOException;
  */
 public class STAR {
 	public static final String LOGIN_URL = "https://isis.sa.ucsb.edu/STAR/";
+	private static final String LOGIN_FORM_ID = "ctl00";
 	
 	
 	STAR(String username, String password) {
 		
 	}
 	
-	public static String login(String username, String password) throws IOException {
+	public static String login(String username, String password) throws IOException, XMLParser.XMLException {
 		HttpClient client = new DefaultHttpClient();
 		
 		HttpGet getRequest = new HttpGet(LOGIN_URL);
 		HttpResponse response = client.execute(getRequest);
 		String content = Utils.toString(response);
-		return content;
+		
+		Document document = XMLParser.getDocumentFromString(content);
+		Element element = document.getElementById(LOGIN_FORM_ID);
+		return element.getAttribute("action");
 	}
 }
--- src/com/nguyenmp/starclient/test/STAR_Test.java
+++ src/com/nguyenmp/starclient/test/STAR_Test.java
@@ -1,6 +1,7 @@
 package com.nguyenmp.starclient.test;
 
 import com.nguyenmp.starclient.STAR;
+import com.nguyenmp.starclient.XMLParser;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,7 +9,7 @@ import java.io.IOException;
 
 public class STAR_Test {
 	@Test
-	public void login_Test() throws IOException {
+	public void login_Test() throws IOException, XMLParser.XMLException {
 		//TODO: Write realistic test for logging in
 		Assert.assertEquals(STAR.login("mpnasf8", "asoidfu"), null);
 	}
