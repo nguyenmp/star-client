758bed9dbff26c4cd7eb1d44a717bef64b199178
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-08-28 08:57:22 -0700

    TODO: Write realistic test for logging in

--- src/com/nguyenmp/starclient/test/STAR_Test.java
+++ src/com/nguyenmp/starclient/test/STAR_Test.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class STAR_Test {
 	@Test
 	public void login_Test() throws IOException {
+		//TODO: Write realistic test for logging in
 		Assert.assertEquals(STAR.login("mpnasf8", "asoidfu"), null);
 	}
 }
