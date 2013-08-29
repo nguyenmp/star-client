9652c2b96a9008aa72186ee422fd0127f63d6ac2
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-08-29 10:06:08 -0700

    Working state of logging in (Redirect to /STAR/

--- src/com/nguyenmp/starclient/STAR.java
+++ src/com/nguyenmp/starclient/STAR.java
@@ -11,26 +11,25 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.junit.Assert;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * A facade that handles all the complicated HTTP calls involved with the STAR API.
  */
 public class STAR {
-	public static final String USERNAME = null, PASSWORD = null;
 	public static final String URL_LOGIN = "https://isis.sa.ucsb.edu/STAR/";
 	public static final String URL_ROOT = "https://sso.my.ucsb.edu";
 	private static final String ID_LOGIN_FORM = "ctl00";
-	
-	
-	STAR(String username, String password) {
-		
-	}
 	
 	public static String login(String username, String password) throws IOException, XMLParser.XMLException {
 		HttpContext context = new BasicHttpContext();
@@ -76,20 +75,56 @@ public class STAR {
 		
 		// Read the response which contains a redirect to the actual site
 		HttpResponse postResponse = client.execute(post, context);
-		post.abort();
-		
-		// Follow redirect
-		getRequest = new HttpGet(URL_LOGIN);
-		postResponse = client.execute(getRequest, context);
-		
 		String redirectHTML = Utils.toString(postResponse);
+		String secondRedirectHTML = followSSORedirect(redirectHTML,client, context);
 		
+		// Test client
+		HttpGet get = new HttpGet(URL_LOGIN);
+		response = client.execute(get, context);
 		
-		
-		return redirectHTML;
+		// Follow second redirect
+		return Utils.toString(response);
 	}
+	
+	public static String followSSORedirect(String redirectHTML, HttpClient client, HttpContext context) throws XMLParser.XMLException, IOException {
+		// Get redirect url
+		int redirectURLStart = redirectHTML.indexOf("<a href=\"") + "<a href=\"".length();
+		int redirectURLEnd = redirectHTML.indexOf("\">", redirectURLStart);
+		String redirectURL = URL_ROOT + StringEscapeUtils.unescapeHtml4(redirectHTML.substring(redirectURLStart, redirectURLEnd));
 
+		// Follow redirect
+		HttpGet getRequest = new HttpGet(redirectURL);
+		HttpResponse postResponse = client.execute(getRequest, context);
+		redirectHTML = Utils.toString(postResponse);
+		
+		Document redirectDocument = XMLParser.getDocumentFromString(redirectHTML);
+		Node bodyNode = XMLParser.getChildFromName(redirectDocument.getDocumentElement(), "body");
+		Node redirectFormNode = XMLParser.getChildFromName((Element) bodyNode, "form");
+		String redirectAction = StringEscapeUtils.unescapeHtml4(((Element) redirectFormNode).getAttribute("action"));
+		String redirectWaValue = StringEscapeUtils.unescapeHtml4(XMLParser.getChildFromAttribute((Element) redirectFormNode, "name", "wa").getAttribute("value"));
+		String redirectWresultValue = StringEscapeUtils.unescapeHtml4(XMLParser.getChildFromAttribute((Element) redirectFormNode, "name", "wresult").getAttribute("value"));
+		String redirectWctxValue = StringEscapeUtils.unescapeHtml4(XMLParser.getChildFromAttribute((Element) redirectFormNode, "name", "wctx").getAttribute("value"));
+
+		List<NameValuePair> redirectNameValuePairs = new ArrayList<NameValuePair>();
+		redirectNameValuePairs.add(new BasicNameValuePair("wa", redirectWaValue));
+		redirectNameValuePairs.add(new BasicNameValuePair("wresult", redirectWresultValue));
+		redirectNameValuePairs.add(new BasicNameValuePair("wctx", redirectWctxValue));
+		UrlEncodedFormEntity redirectEntity = new UrlEncodedFormEntity(redirectNameValuePairs);
+
+		HttpPost redirectPost = new HttpPost(redirectAction);
+		redirectPost.setEntity(redirectEntity);
+		
+		HttpResponse redirectResponse = client.execute(redirectPost, context);
+		return Utils.toString(redirectResponse);
+	}
+	
 	public static void main(String[] args) throws IOException, XMLParser.XMLException {
-		System.out.println(STAR.login(USERNAME, PASSWORD));
+		System.out.println(new File("credentials.conf").getAbsolutePath());
+		Scanner scanner = new Scanner(new FileInputStream("credentials.conf"));
+		String username = scanner.nextLine();
+		String password = scanner.nextLine();
+
+		//TODO: Write realistic test for logging in
+		Assert.assertEquals(STAR.login(username, password), null);
 	}
 }
