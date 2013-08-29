d4c570213152348fec8e26a2c95fe8506f6453e8
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-08-29 10:05:33 -0700

    Moved credentials to credentials.conf file

--- src/com/nguyenmp/starclient/test/STAR_Test.java
+++ src/com/nguyenmp/starclient/test/STAR_Test.java
@@ -5,12 +5,18 @@ import com.nguyenmp.starclient.XMLParser;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class STAR_Test {
 	@Test
 	public void login_Test() throws IOException, XMLParser.XMLException {
+		Scanner scanner = new Scanner(new FileInputStream("credentials.conf"));
+		String username = scanner.nextLine();
+		String password = scanner.nextLine();
+		
 		//TODO: Write realistic test for logging in
-		Assert.assertEquals(STAR.login("mpnasf8", "asoidfu"), null);
+		Assert.assertEquals(STAR.login(username, password), null);
 	}
 }
